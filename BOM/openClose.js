107da36042310fd4aaa2c4ce3a3286a9844a401d
Hamser1001 <upscale.mind50@gmail.com>
2024-07-06 19:56:46 +0100

    window feature

---
+++ BOM/openClose.js
@@ -0,0 +1,19 @@
+/*
+  BOM [Browser Object Model]
+  - open(URL [Opt], Window Name Or Target Attr [Opt], Win Features [Opt], History Replace [Opt])
+  - close()
+  - Window Features
+  --- left [Num]
+  --- top [Num]
+  --- width [Num]
+  --- height [Num]
+  --- menubar [yes || no]
+
+  Search
+  - Window.Open Window Features
+*/
+
+setTimeout(function () {
+    window.open("https://google.com", "_blank", "");
+}, 2000);
+
