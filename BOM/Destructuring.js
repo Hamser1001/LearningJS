fa7bdf680630c4daf197fbcac29b12cb11e4d36b
Hamser1001 <upscale.mind50@gmail.com>
2024-07-07 23:39:36 +0100

    destructring 1

---
+++ BOM/Destructuring.js
@@ -0,0 +1,14 @@
+/*
+  Destructuring
+  " is a JavaScript expression that allows us to extract data from arrays,
+    objects, and maps and set them into new, distinct variables. "
+  - Destructuring Array
+*/
+
+let myFriends = ["Ahmed", "Sayed", "Ali", "Maysa"];
+let [a, b, c, d] = myFriends;
+console.log(a);
+console.log(b);
+
+console.log(c);
+console.log(d);
