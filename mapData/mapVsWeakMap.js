61279824673521bb00d6b4127d22078250b82ca9
Hamser1001 <upscale.mind50@gmail.com>
2024-07-11 00:16:48 +0100

    map vs weakMap

---
+++ mapData/mapVsWeakMap.js
@@ -0,0 +1,10 @@
+/*
+  - Map vs WeakMap
+  "
+    WeakMap Allows Garbage Collector To Do Its Task But Not Map.
+  "
+  --
+  Map     => Key Can Be Anything
+  WeakMap => Key Can Be Object Only
+  --
+*/
