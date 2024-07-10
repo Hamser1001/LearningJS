57abe3226252dbfa21ec3f91ca97bf7273587f1d
Hamser1001 <upscale.mind50@gmail.com>
2024-07-11 00:48:06 +0100

    array from

---
+++ array/arrayMethods.js
@@ -0,0 +1,28 @@
+/*
+  Array Methods
+  - Array.from(Iterable, MapFunc, This)
+  --- Variable
+  --- String Numbers
+  --- Set
+  --- Using The Map Function
+  --- Arrow Function
+  --- Shorten The Method + Use arguments
+*/
+
+console.log(Array.from("Hamza"));
+console.log(Array.from("12345", function (n) {
+    return +n + +n;
+}));
+
+console.log(Array.from("12345", (n) => +n + +n));
+
+let myArray = [1, 1, 1, 2, 3, 4];
+// let mySet = new Set(myArray);
+// console.log(Array.from(mySet));
+
+// console.log([...new Set(myArray)]); // Future
+
+function af() {
+    return Array.from(arguments);
+};
+console.log(af("Hamza", "Ahmed", "Sayed"));
