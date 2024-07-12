2d4d0174cdd3535ce1be08eaa6d20908c118f246
Hamser1001 <upscale.mind50@gmail.com>
2024-07-12 20:54:55 +0100

    regular expression

---
+++ regular expression/regular.js
@@ -0,0 +1,20 @@
+/*
+  Regular Expression
+  - Email
+  - IP
+  - Phone
+  - URL
+*/
+
+let str1 = "10 20 100 1000 5000";
+let str2 = "Os1 Os12 Os123 Os123Os Os12312Os123";
+
+let invalidEmail = "Hamza@@@gmail...com";
+let validEmail = "o@nn.sa";
+let ip = "192.168.2.1"; // Ipv4
+
+let url = "google.com";
+url = "google.com/";
+url = "www.google.com";
+url = "www.google.com/";
+url = "http://www.google.com";
