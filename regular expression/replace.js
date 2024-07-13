a5f7a8450334f3a7db6e094e37d7d93c099652b7
Hamser1001 <upscale.mind50@gmail.com>
2024-07-13 09:07:08 +0100

    replaceand replace all regex

---
+++ regular expression/replace.js
@@ -0,0 +1,12 @@
+/*
+  Regular Expression
+
+  - replace
+  - replaceAll
+*/
+
+let txt = "We Love Programming And @ Because @ Is Amazing";
+console.log(txt.replace("@", "JavaScript"));
+console.log(txt.replaceAll("@", "JavaScript"));
+let regex = /@/ig;
+console.log(txt.replaceAll(regex, "JavaScript"));
