4729253613016cc3342fa7bdee90adaaa116fd0d
Hamser1001 <upscale.mind50@gmail.com>
2024-07-13 07:10:02 +0100

    charClasses

---
+++ regular expression/charClasses1.js
@@ -0,0 +1,17 @@
+/*
+  Regular Expression
+  Character Classes
+  \b => matches at the beginning or end of a word.
+  \B => matches NOT at the beginning/end of a word.
+
+  Test Method
+  pattern.test(input)
+*/
+
+let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo";
+let re = /(\bspam|spam\b)/ig;
+console.log(names.match(re));
+console.log(re.test(names));
+console.log(re.test("Hamza"));
+console.log(re.test("1Spam"));
+console.log(/(\bspam|spam\b)/ig.test("Spam1"));
