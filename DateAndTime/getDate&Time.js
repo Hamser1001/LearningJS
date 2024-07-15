1d57eb69ef966085e9300e34d79b7b63cab66c49
Hamser1001 <upscale.mind50@gmail.com>
2024-07-15 20:28:20 +0100

    get date & time

---
+++ DateAndTime/getDate&Time.js
@@ -0,0 +1,26 @@
+/*
+  Date And Time
+  - getTime() => Number Of Milliseconds
+  - getDate() => Day Of The Month
+  - getFullYear()
+  - getMonth() => Zero Based
+  - getDay() => Day Of The Week
+  - getHours()
+  - getMinutes()
+  - getSeconds()
+*/
+
+let dateNow = new Date();
+let birthday = new Date("Apr 14, 1994");
+let datDiff = dateNow - birthday;
+console.log(datDiff);
+console.log(datDiff / 1000 / 60 / 60 / 24 / 365);
+console.log(dateNow);
+console.log(dateNow.getTime());
+console.log(dateNow.getDate());
+console.log(dateNow.getFullYear());
+console.log(dateNow.getMonth());
+console.log(dateNow.getDay());
+console.log(dateNow.getHours());
+console.log(dateNow.getMinutes());
+console.log(dateNow.getSeconds());
