57a0fc0a84caf79e199ddf7b2615db6eefd313ab
Hamser1001 <upscale.mind50@gmail.com>
2024-07-17 01:23:23 +0100

    default export named export all

---
+++ Generator/export.js
@@ -0,0 +1,19 @@
+/*
+  Modules
+  - Export Alias
+  - Named Export
+  - Default Export
+  - Import All
+*/
+
+let a = 10;
+let arr = [1, 2, 3, 4];
+
+function saySomething() {
+    return `Something`;
+};
+
+export {a as myNumber, arr, saySomething};
+export default function sayHello() {
+    return `Hello`;
+};
---
+++ Generator/exportApp.js
@@ -0,0 +1,12 @@
+// import sayHello ,{ myNumber, arr, saySomething as s } from "./export.js";
+
+// console.log(myNumber);
+// console.log(arr);
+// console.log(s());
+// console.log(sayHello());
+
+
+import * as all from "./export.js";
+console.log(all);
+
+console.log(all.myNumber);
